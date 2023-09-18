244378f33c37b9263ea1c0cdf94a807b0a4857ff
Ameen-vv <mohammedameenvv@gmail.com>
2023-09-18 13:43:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationPageComponent,
+    SignInPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
